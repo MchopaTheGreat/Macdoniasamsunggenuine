a3f6a78e14f00a801ceed8f8095fdb7e5b49612a
MchopaTheGreat <143799790+MchopaTheGreat@users.noreply.github.com>
2023-09-16 13:45:04 +0300

    Add files via upload

---
+++ real font.php
@@ -0,0 +1,344 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<script src="https://kit.fontawesome.com/2a56057606.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<!-- remeve icon x mark -->
+<i class="fa-solid fa-xmark"></i>
+<i class="fa-regular fa-xmark"></i>
+<i class="fa-light fa-xmark"></i>
+
+<!-- remove rounded icon -->
+<i class="fa-solid fa-circle-xmark"></i>
+<i class="fa-regular fa-circle-xmark"></i>
+
+<!-- tiki icom -->
+<i class="fa-solid fa-check"></i>
+<i class="fa-light fa-check"></i>
+<i class="fa-thin fa-check"></i>
+
+<!-- alama ya mshangao ndani ya duara -->
+<i class="fa-solid fa-circle-exclamation"></i>
+<i class="fa-regular fa-circle-exclamation"></i>
+<i class="fa-thin fa-circle-exclamation"></i>
+
+<!-- alama ya mshangano ndani ya pembe tatu -->
+<i class="fa-solid fa-triangle-exclamation"></i>
+<i class="fa-regular fa-triangle-exclamation"></i>
+
+<!-- trash icom (dustbean) -->
+<i class="fa-solid fa-trash"></i>
+<i class="fa-regular fa-trash"></i>
+<i class="fa-thin fa-trash"></i>
+
+<!-- trash check -->
+<i class="fa-solid fa-trash-xmark"></i>
+<i class="fa-solid fa-trash-undo"></i>
+<i class="fa-solid fa-trash-slash"></i>
+<i class="fa-solid fa-trash-plus"></i>
+<i class="fa-solid fa-trash-clock"></i>
+<i class="fa-solid fa-trash-check"></i>
+
+<!-- adding user -->
+<i class="fa-solid fa-user-plus"></i>
+<i class="fa-regular fa-user-plus"></i>
+<i class="fa-light fa-user-plus"></i>
+
+<!-- remove user -->
+<i class="fa-solid fa-user-minus"></i>
+<i class="fa-light fa-user-minus"></i>
+<i class="fa-thin fa-user-minus"></i>
+
+<!-- graduation cape -->
+<i class="fa-solid fa-graduation-cap"></i>
+<i class="fa-regular fa-graduation-cap"></i>
+<i class="fa-light fa-graduation-cap"></i>
+
+<!-- hosptial user -->
+<i class="fa-solid fa-hospital-user"></i>
+<i class="fa-regular fa-hospital-user"></i>
+<i class="fa-light fa-hospital-user"></i>
+
+<!-- user medical -->
+<i class="fa-solid fa-users-medical"></i>
+<i class="fa-thin fa-users-medical"></i>
+
+<!-- clipboard medical -->
+<i class="fa-regular fa-clipboard-medical"></i>
+<i class="fa-solid fa-clipboard-medical"></i>
+
+<!-- user doctor -->
+<i class="fa-solid fa-user-doctor"></i>
+<i class="fa-light fa-user-doctor"></i>
+<i class="fa-duotone fa-user-doctor"></i>
+<i class="fa-solid fa-user-doctor-hair-long"></i>
+
+<!-- message to the doctor -->
+<i class="fa-regular fa-user-doctor-message"></i>
+<i class="fa-duotone fa-user-doctor-message"></i>
+
+<!-- nurse user -->
+<i class="fa-duotone fa-user-nurse"></i>
+<i class="fa-sharp fa-solid fa-user-nurse"></i>
+
+<!-- first aid briefcase -->
+<i class="fa-solid fa-briefcase-medical"></i>
+<i class="fa-regular fa-briefcase-medical"></i>
+
+<!-- sindano icon -->
+<i class="fa-sharp fa-light fa-syringe"></i>
+<i class="fa-regular fa-syringe"></i>
+
+<!-- calendar -->
+<i class="fa-sharp fa-solid fa-calendar-days"></i>
+<i class="fa-sharp fa-regular fa-calendar-days"></i>
+
+<!-- add calendar (add appointment) -->
+<i class="fa-solid fa-calendar-circle-plus"></i>
+
+<!-- remove calendar (removel appointment) -->
+<i class="fa-solid fa-calendar-circle-minus"></i>
+
+<!-- appointment with user  -->
+<i class="fa-sharp fa-solid fa-calendar-users"></i>
+<i class="fa-sharp fa-regular fa-calendar-users"></i>
+
+<!-- edit user -->
+<i class="fa-solid fa-pen-to-square"></i>
+<i class="fa-regular fa-pen-to-square"></i>
+<i class="fa-regular fa-pen-to-square"></i>
+
+<!-- view icon (jicho) -->
+<i class="fa-solid fa-eye"></i>
+<i class="fa-regular fa-eye"></i>
+<i class="fa-light fa-eye"></i>
+
+<!-- you can't view (jicho limefumba) -->
+<i class="fa-solid fa-eye-slash"></i>
+<i class="fa-regular fa-eye-slash"></i>
+<i class="fa-light fa-eye-slash"></i>
+
+<!-- menu bar -->
+<i class="fa-sharp fa-solid fa-bars"></i>
+<i class="fa-sharp fa-regular fa-bars"></i>
+<i class="fa-sharp fa-light fa-bars"></i>
+
+<!-- books -->
+<i class="fa-duotone fa-book"></i>
+<i class="fa-sharp fa-solid fa-book"></i>
+
+<!-- book user (author) -->
+<i class="fa-solid fa-book-user"></i>
+<i class="fa-sharp fa-solid fa-book-user"></i>
+
+<!-- contact (phone icon) -->
+<i class="fa-solid fa-phone"></i>
+<i class="fa-regular fa-phone"></i>
+<i class="fa-light fa-phone"></i>
+
+<!-- call icon -->
+<i class="fa-solid fa-phone-volume"></i>
+<i class="fa-regular fa-phone-volume"></i>
+<i class="fa-light fa-phone-volume"></i>
+
+<!-- home font -->
+<i class="fa-solid fa-house-chimney"></i>
+<i class="fa-duotone fa-house-chimney"></i>
+
+<!-- user font -->
+<i class="fa-solid fa-user"></i>
+<i class="fa-duotone fa-user"></i>
+<i class="fa-solid fa-users"></i>
+<i class="fa-regular fa-user"></i>
+
+<!-- galley -->
+<i class="fa-solid fa-gallery-thumbnails"></i>
+<i class="fa-regular fa-gallery-thumbnails"></i>
+<i class="fa-light fa-gallery-thumbnails"></i>
+
+<!-- news letter -->
+<i class="fa-solid fa-newspaper"></i>
+<i class="fa-regular fa-newspaper"></i>
+<i class="fa-light fa-newspaper"></i>
+<i class="fa-thin fa-newspaper"></i>
+
+<!-- camera -->
+<i class="fa-solid fa-camera-retro"></i>
+<i class="fa-light fa-camera"></i>
+<i class="fa-thin fa-camera"></i>
+
+<!-- donation -->
+<i class="fa-duotone fa-sack-dollar"></i>
+<i class="fa-duotone fa-sack"></i>
+
+<!-- newsletter open -->
+<i class="fa-solid fa-envelope-open-text"></i>
+<i class="fa-thin fa-envelope-open-text"></i>
+<i class="fa-duotone fa-envelope-open-text"></i>
+
+<!-- mbegu ndani ya begi -->
+<i class="fa-duotone fa-bag-seedling"></i>
+<i class="fa-solid fa-bag-seedling"></i>
+
+<!-- password font -->
+<i class="fa-regular fa-lock"></i>
+<i class="fa-solid fa-lock"></i>
+<i class="fa-light fa-lock"></i>
+<i class="fa-thin fa-lock"></i>
+<i class="fa-solid fa-unlock"></i>
+<i class="fa-regular fa-unlock"></i>
+<i class="fa-light fa-unlock"></i>
+<i class="fa-thin fa-unlock"></i>
+
+
+<!-- key font -->
+<i class="fa-solid fa-key"></i>
+<i class="fa-regular fa-key"></i>
+<i class="fa-light fa-key"></i>
+<i class="fa-thin fa-key"></i>
+<i class="fa-duotone fa-key"></i>
+
+<!-- confirm password(tiki) -->
+<i class="fa-solid fa-check"></i>
+<i class="fa-regular fa-check"></i>
+<i class="fa-light fa-check"></i>
+<i class="fa-thin fa-check"></i>
+
+<!-- mshangao font -->
+<i class="fa-solid fa-circle-exclamation"></i>
+<i class="fa-regular fa-circle-exclamation"></i>
+<i class="fa-light fa-circle-exclamation"></i>
+<i class="fa-thin fa-circle-exclamation"></i>
+
+<!-- login font -->
+<i class="fa-regular fa-right-to-bracket"></i>
+<i class="fa-solid fa-right-to-bracket"></i>
+<i class="fa-light fa-right-to-bracket"></i>
+<i class="fa-thin fa-right-to-bracket"></i>
+
+
+<!-- email font -->
+<i class="fa-solid fa-envelope"></i>
+<i class="fa-regular fa-envelope"></i>
+<i class="fa-sharp fa-regular fa-envelope"></i>
+
+<!-- bar font -->
+<i class="fa-solid fa-bars"></i>
+<i class="fa-regular fa-bars"></i>
+<i class="fa-sharp fa-solid fa-bars"></i>
+
+<!-- phone font -->
+<i class="fa-solid fa-phone"></i>
+<i class="fa-regular fa-phone"></i>
+<i class="fa-duotone fa-phone-volume"></i>
+<i class="fa-solid fa-phone-volume"></i>
+<i class="fa-solid fa-phone-rotary"></i>
+<i class="fa-light fa-phone-missed"></i>
+
+<!-- facebook font -->
+<i class="fa-brands fa-facebook"></i>
+<i class="fa-brands fa-facebook-f"></i>
+<i class="fa-brands fa-square-facebook"></i>
+
+<!-- whatsapp font -->
+<i class="fa-brands fa-whatsapp"></i>
+<i class="fa-brands fa-square-whatsapp"></i>
+
+<!-- instagram font -->
+<i class="fa-brands fa-instagram"></i>
+<i class="fa-brands fa-square-instagram"></i>
+
+<!-- twitter font -->
+<i class="fa-brands fa-twitter"></i>
+<i class="fa-brands fa-square-twitter"></i>
+
+<!-- glober font -->
+<i class="fa-solid fa-globe"></i>
+<i class="fa-regular fa-globe"></i>
+<i class="fa-light fa-globe"></i>
+<i class="fa-solid fa-earth-americas"></i>
+<i class="fa-regular fa-earth-americas"></i>
+
+<!-- adress card -->
+<i class="fa-solid fa-address-card"></i>
+<i class="fa-regular fa-address-card"></i>
+<i class="fa-light fa-address-card"></i>
+<i class="fa-thin fa-address-card"></i>
+<i class="fa-duotone fa-address-card"></i>
+
+<!-- payment card -->
+<i class="fa-solid fa-credit-card"></i>
+<i class="fa-regular fa-credit-card"></i>
+<i class="fa-light fa-credit-card"></i>
+<i class="fa-duotone fa-credit-card"></i>
+
+<!-- search font -->
+<i class="fa-solid fa-magnifying-glass"></i>
+<i class="fa-regular fa-magnifying-glass"></i>
+<i class="fa-light fa-magnifying-glass"></i>
+
+<!-- calendar font -->
+<i class="fa-sharp fa-solid fa-calendar-days"></i>
+<i class="fa-sharp fa-light fa-calendar-days"></i>
+
+
+<!-- watch date -->
+<i class="fa-regular fa-alarm-clock"></i>
+<i class="fa-sharp fa-solid fa-alarm-clock"></i>
+
+<!-- send font -->
+<i class="fa-solid fa-paper-plane"></i>
+<i class="fa-regular fa-paper-plane"></i>
+<i class="fa-light fa-paper-plane"></i>
+
+<!-- logout font -->
+<i class="fa-regular fa-up-from-bracket"></i>
+<i class="fa-sharp fa-light fa-arrow-up-from-bracket"></i>
+<i class="fa-sharp fa-solid fa-up-from-bracket"></i>
+
+
+<!-- bell font -->
+<i class="fa-solid fa-bell"></i>
+<i class="fa-regular fa-bell"></i>
+<i class="fa-regular fa-bells"></i>
+<i class="fa-solid fa-bells"></i>
+<i class="fa-regular fa-bell-slash"></i>
+<i class="fa-light fa-bells"></i>
+<i class="fa-sharp fa-regular fa-bell-slash"></i>
+
+<!-- bulb font -->
+<i class="fa-regular fa-lightbulb-on"></i>
+<i class="fa-sharp fa-light fa-lightbulb-on"></i>
+<i class="fa-regular fa-lightbulb-exclamation-on"></i>
+<i class="fa-solid fa-lightbulb-exclamation-on"></i>
+
+
+<!-- code font -->
+<i class="fa-solid fa-code"></i>
+<i class="fa-regular fa-code"></i>
+<i class="fa-light fa-code"></i>
+
+<!-- another font -->
+<i class="fa-solid fa-handshake"></i>
+<i class="fa-brands fa-tiktok"></i>
+<i class="fa-solid fa-download"></i>
+<i class="fa-solid fa-camera-retro"></i>
+<i class="fa-solid fa-print"></i>
+<i class="fa-solid fa-bookmark"></i>
+<i class="fa-solid fa-gift"></i>
+<i class="fa-solid fa-thumbs-up"></i>
+<i class="fa-solid fa-layer-group"></i>
+<i class="fa-solid fa-address-book"></i>
+<i class="fa-solid fa-upload"></i>
+
+
+
+</body>
+</html>
